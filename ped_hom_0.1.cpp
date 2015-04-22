4697a32a63326385a4e57fb78779a738060008be
Matthew Teasdale <matt@teasdale.xyz>
2015-04-22 17:02:29 +0100

    Update ped_hom_0.1.cpp

--- ped_hom_0.1.cpp
+++ ped_hom_0.1.cpp
@@ -105,7 +105,7 @@ int main (int argc, char* argv[]) {
   /*
    * Simple main statement to show programs name and collect commands
   */
-  std::cerr << "ped_hom v0.1 (21 March 2015)  https://github.com/teasdalm/ped_hom" << std::endl;
+  std::cerr << "ped_hom v0.1 (22 April 2015)  https://github.com/teasdalm/ped_hom" << std::endl;
   std::cerr << "(C) 2015 Matthew Teasdale  GNU General Public License v3" << std::endl;
 
   if(argc < 2){
